8a6a27c142b8b6c78da4fad25fbe044bc47fd71e
Andrew Marine <andrewmarine7882@gmail.com>
2018-09-19 00:44:35 -0700

    Set up proxy redirection to modual api's

--- server/index.js
+++ server/index.js
@@ -5,9 +5,26 @@ const path = require('path');
 const port = process.env.PORT || 3030;
 
 const server = express();
+const router = express.Router();
 
 server.use(cors());
-server.use(express.static(path.join(__dirname, '../public')));
+server.use(
+  '/businesses/:id',
+  express.static(path.join(__dirname, '../public')),
+);
+server.use('/api/businesses', router);
+
+router.all('/:id/images*', (req, res) => {
+  res.redirect(307, `http://localhost:3000${req.url}`);
+});
+
+router.all('/:id/reviews*', (req, res) => {
+  res.redirect(307, `http://localhost:3002${req.url}`);
+});
+
+router.all('/:id/restaurantInfo*', (req, res) => {
+  res.redirect(307, `http://localhost:8080${req.url}`);
+});
 
 server.listen(port, () => {
   console.log('listening on 3030');
